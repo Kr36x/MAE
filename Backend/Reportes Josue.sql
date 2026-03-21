a1788f1b663d61a3036d20745da5f5f78f766e9e
Josue Varela <jdvarelas@unah.hn>
2026-03-20 20:28:01 -0600

    calificaciones y correicion de procedimientos

--- Backend/Reportes Josue.sql
+++ Backend/Reportes Josue.sql
@@ -3,23 +3,14 @@ use AgroLinkDB
 select * from CargaAcademica
 select * from Grado
 
---1.- Asistencias diariasas
-select D.Nombre as Docente,G.Nivel as Grado,S.Letra as Seccion,A.Fecha as Fecha,E.Nombre as Estudiante, A.Estado as Estado,
-isnull( A.Observacion,'Sin observaciones') as Observaciones
-from Asistencia A inner join CargaAcademica CA on A.CargaID=CA.CargaID 
-inner join Estudiante E on A.EstudianteID=E.EstudianteID
-inner join Seccion S on CA.SeccionID=S.SeccionID
-inner join Grado G on G.GradoID=S.GradoID
-inner join Docente D on CA.DocenteID=d.DocenteID
-where a.Fecha between '2026-03-02' and '2026-03-03' and d.Nombre='JUAN PEREZ'
-order by a.Fecha ASC,E.Nombre ASC
+--2.- Asistencias diariasas
 
 go
 create or alter procedure spMAE_Asistencias_por_Grado @fecha_inicial DATE,@fecha_final DATE = NULL,@Docente varchar(100),@Estudiante varchar(100)=Null 
 as
 begin
 
-select D.Nombre as Docente,G.Nivel as Grado,S.Letra as Seccion,A.Fecha as Fecha,E.Nombre as Estudiante, A.Estado as Estado,
+select D.Nombre as Docente,G.NombreGrado as Grado,S.Letra as Seccion,A.Fecha as Fecha,E.Nombre as Estudiante, A.Estado as Estado,
 isnull( A.Observacion,'Sin observaciones') as Observaciones
 from Asistencia A inner join CargaAcademica CA on A.CargaID=CA.CargaID 
 inner join Estudiante E on A.EstudianteID=E.EstudianteID
@@ -43,4 +34,42 @@ go
 EXEC spMAE_Asistencias_por_Grado @fecha_inicial='2026-03-02',@fecha_final='2026-03-04',@Docente='JUAN PEREZ', @Estudiante='Alexis'
 
 
+-- 3.- calificacioens semanales
 
+select E.EstudianteID,E.Nombre as Estudiante,G.NombreGrado as Grado,s.Letra as Seccion,A.Descripcion,
+ASI.Nombre as Asignatura,A.Parcial,A.Valor as Valor,C.Nota as calificacion,Year(C.Fecha) as Academic_Year from 
+Calificacion C inner join Actividad A on C.ActividadID=A.ActividadID 
+inner join CargaAcademica CA on A.CargaID=CA.CargaID
+inner join Seccion S on CA.SeccionID=S.SeccionID
+inner join Grado G on S.GradoID=G.GradoID
+inner join Estudiante E on C.EstudianteID=E.EstudianteID
+inner join Asignatura Asi on CA.AsignaturaID=Asi.AsignaturaID
+where g.NombreGrado='PRIMERO' and a.Parcial= 1 and s.letra='A' and asi.Nombre like '%Math%' and year(c.Fecha) = 2026
+
+
+
+go
+create or alter procedure spMAE_Calificaciones_semanlaes @Grado Varchar(50),@parcial int,@Seccion varchar(10),@Asignatura varchar(70),@anio int
+,@Estudiante varchar(100)=Null
+as
+begin
+
+select E.EstudianteID,E.Nombre as Estudiante,G.NombreGrado as Grado,s.Letra as Seccion,A.Descripcion,
+ASI.Nombre as Asignatura,A.Parcial,A.Valor as Valor,C.Nota as calificacion from 
+Calificacion C inner join Actividad A on C.ActividadID=A.ActividadID 
+inner join CargaAcademica CA on A.CargaID=CA.CargaID
+inner join Seccion S on CA.SeccionID=S.SeccionID
+inner join Grado G on S.GradoID=G.GradoID
+inner join Estudiante E on C.EstudianteID=E.EstudianteID
+inner join Asignatura Asi on CA.AsignaturaID=Asi.AsignaturaID
+where g.NombreGrado=@Grado and 
+a.Parcial= @parcial 
+and s.letra=@Seccion 
+and asi.Nombre like '%' + @Asignatura + '%' 
+and year(c.Fecha) = @anio
+and (@Estudiante IS NULL OR E.Nombre LIKE '%' + @Estudiante + '%')
+
+end
+
+go
+EXEC spMAE_Calificaciones_semanlaes @Grado='Primero',@parcial= 1,@Seccion='A',@Asignatura = 'Math',@anio =2026
