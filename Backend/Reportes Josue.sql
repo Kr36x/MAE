161372cd6b86184c1d4399a518f3a6e83879ea09
Josue Varela <jdvarelas@unah.hn>
2026-03-21 15:47:07 -0600

    filtrar seccion y grado

--- Backend/Reportes Josue.sql
+++ Backend/Reportes Josue.sql
@@ -6,7 +6,8 @@ select * from Grado
 --2.- Asistencias diariasas
 
 go
-create or alter procedure spMAE_Asistencias_por_Grado @fecha_inicial DATE,@fecha_final DATE = NULL,@Docente varchar(100),@Estudiante varchar(100)=Null 
+create or alter procedure spMAE_Asistencias_por_Grado @fecha_inicial DATE,@fecha_final DATE = NULL,@Docente varchar(100),@Estudiante varchar(100)=Null ,@grado varchar(100),
+@Seccion varchar(10)
 as
 begin
 
@@ -25,13 +26,15 @@ where (
           (@fecha_final IS NOT NULL AND A.Fecha BETWEEN @fecha_inicial AND @fecha_final)
       ) and d.Nombre=@Docente 
       and (@Estudiante IS NULL OR E.Nombre LIKE '%' + @Estudiante + '%')
+      and  g.NombreGrado=@Grado and 
+        s.Letra=@Seccion 
 order by a.Fecha ASC,E.Nombre ASC
 end
 
 go
 
 --probando
-EXEC spMAE_Asistencias_por_Grado @fecha_inicial='2026-03-02',@fecha_final='2026-03-04',@Docente='JUAN PEREZ', @Estudiante='Alexis'
+EXEC spMAE_Asistencias_por_Grado @fecha_inicial='2026-03-02',@fecha_final='2026-03-04',@Docente='JUAN PEREZ',@Grado='Segundo',@Seccion='A',@Estudiante='ANGEL GABRIEL RIVERA'
 
 
 -- 3.- calificacioens semanales
@@ -72,4 +75,4 @@ and (@Estudiante IS NULL OR E.Nombre LIKE '%' + @Estudiante + '%')
 end
 
 go
-EXEC spMAE_Calificaciones_semanlaes @Grado='Primero',@parcial= 1,@Seccion='A',@Asignatura = 'Math',@anio =2026
+EXEC spMAE_Calificaciones_semanlaes @Grado='Primero',@parcial= 1,@Seccion='A',@Asignatura = 'Math',@anio =2026,@Estudiante='BRAYAN JOSUE ESPINAL'
